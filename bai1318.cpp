21d629d598d0a5817257a0691fb8910adfba62f0
danghuy17 <dang.huy.17.14@gmail.com>
2021-10-01 00:32:36 +0700

    commit bai 13.8

---
+++ bai1318.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+int main() {
+    long long odd_number[100], even_number[100], chia8[100];
+    int odd = 0, even = 0, chia = 0;
+    long long n;
+    while(std::cin >> n && n) {
+        if(n % 2) {
+            odd_number[odd] = n;
+            ++odd;
+        }
+        else {
+            even_number[even] = n;
+            ++even;
+        }
+        if(n % 8 ==0) {
+            chia8[chia] = n;
+            ++chia;
+        }
+    }
+    std::cout << "Cac so chan: ";
+    for(int i = 0; i < even; ++i) {
+        std::cout << even_number[i] << " ";
+    }
+    std::cout << std::endl << "Cac so le: ";
+    for(int i = 0; i < odd; ++i) {
+        std::cout << odd_number[i] << " ";
+    }
+    std::cout << std::endl << "Cac so chia het cho 8: ";
+    for(int i = 0; i < chia; ++i) {
+        std::cout << chia8[i] << " ";
+    }
+    return 0;
+}
