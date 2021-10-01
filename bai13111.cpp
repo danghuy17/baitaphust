abee67745ea47a5e932cce5bd54bba8e9d17a0fb
danghuy17 <dang.huy.17.14@gmail.com>
2021-10-01 09:41:52 +0700

    sua bai 13.11

--- bai13111.cpp
+++ bai13111.cpp
@@ -3,20 +3,21 @@
 #include<iomanip>
 #define EPSILON 1e-08
 
-double mySqrt(double number) {
+double mySqrt(int number) {
     double result = 1.0;
     double result1;
-    do{
+    do {
         result1 = result;
         result = (result + number / result) / 2;
     } while(fabs(result - result1) >= EPSILON);
     return result;
+    
 }
 
 int main() {
-    unsigned int number;
-    std::cout << "Nhap gia tri: ";
+    int number;
+    std::cout << "Nhap number: ";
     std::cin >> number;
-    std::cout << std::fixed << std::setprecision(8) << mySqrt(number);
+    std::cout << std::fixed << std::setprecision(8) << "Can bac 2 cua " << number << " la: " << mySqrt(number);
     return 0;
 }
