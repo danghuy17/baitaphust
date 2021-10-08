5c130c13dd270de95ae14e525e063d6b2449d991
danghuy17 <dang.huy.17.14@gmail.com>
2021-10-08 16:31:41 +0700

    sua bai 13.2

--- bai132.cpp
+++ bai132.cpp
@@ -1,16 +1,28 @@
 #include<iostream>
 
-long long XmuN(long long x, long long n) {
+double XmuN(long long x, long long n) {
     if(n == 0)
         return 1;
     return XmuN(x, n - 1) * x;
 }
 
 int main() {
-    long long x, n;
+    long long n;
+    double x;
+    bool check = true;
+
     std::cout << "Nhap gia tri x va n: ";
     std::cin >> x >> n;
 
-    std::cout << XmuN(x, n);
+    if (n < 0)
+        check = false;
+
+    n = (n > 0) ? n : -n;
+
+    if (check == true)
+        std::cout << XmuN(x, n);
+    else
+        std::cout << 1 / XmuN(x, n);
+
     return 0;
 }
