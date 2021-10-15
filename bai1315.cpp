f330cb609ffff14a2b9bc100a0bedbdc5a53b13e
danghuy17 <dang.huy.17.14@gmail.com>
2021-10-15 15:56:38 +0700

    bai 13.15 bo sung doi sang he 16

--- bai1315.cpp
+++ bai1315.cpp
@@ -1,13 +1,62 @@
 #include<iostream>
 #include<iomanip>
+
+char kytu(int x) {
+    switch(x) {
+        case 10: {
+            return 'A';
+        }
+        case 11: {
+            return 'B';
+        }
+        case 12: {
+            return 'C';
+        }
+        case 13: {
+            return 'D';
+        }
+        case 14: {
+            return 'E';
+        }
+        case 15: {
+            return 'F';
+        }
+        default: {
+            return char(x + 48);
+        }
+    }
+}
+
+std::string he16(int x) {
+    std::string s = "0x";
+
+    int a = (x & 240) >> 4;
+
+    int b = x & 15;
+
+    s = s + kytu(a);
+
+    s = s + kytu(b);
+
+    return s;
+}
+
+
 int main() {
     std::cout << "Bang chu cai in hoa" << std::endl;
+
     for(int i = 'A'; i <= 'Z'; ++i) {
-        std::cout << char(i) << " " << std::setfill('0') << std::setw(3) << i << std::endl;
+        std::cout << char(i) << " " << std::setfill('0') << std::setw(3) << i << " " << he16(i) << std::endl;
     }
+
     std::cout << "Bang chu cai in thuong" << std::endl;
+
     for(int i = 'a'; i <= 'z'; ++i) {
-        std::cout << char(i) << " " << std::setfill('0') << std::setw(3) << i << std::endl;
+        std::cout << char(i) << " " << std::setfill('0') << std::setw(3) << i << " " << he16(i) << std::endl;
+    }
+
+    for (int i = '0'; i <= '9'; ++i) {
+        std::cout << char(i) << " " << std::setw(3) << i << " " << he16(i) << std::endl;
     }
     return 0;
 }
