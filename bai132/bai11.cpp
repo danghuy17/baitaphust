b5e20992ea6dd507b16426bec5f8fd0774994dce
danghuy17 <dang.huy.17.14@gmail.com>
2021-11-04 22:43:25 +0700

    bai 11 of 13.2

---
+++ bai132/bai11.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cctype>
+
+std::string chuanHoa(std::string fullName) {
+    std::string new_name = fullName;
+    bool firstChar = true;
+
+    for (int i = 0; fullName[i] != '\0'; ++i) {
+        new_name[i] = tolower(new_name[i]);
+
+        if (firstChar == true) {
+            new_name[i] = new_name[i] - ' ';
+            firstChar = false;
+        }
+        if (new_name[i] == ' ' ) {
+            firstChar = true;
+        }
+    }
+
+    return new_name;
+}
+
+void bai11() {
+    std::string fullName = "bui dang huy";
+
+    std::cout << "Ten da chuan Hoa: " << chuanHoa(fullName) << std:: endl;
+}
+
+int main() {
+    bai11();
+
+    return 0;
+}
